8d71ef7ac8f412b22b736492912835459e6c1284
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-06-18 10:43:13 +0900

    BE 프로젝트 생성

---
+++ src/main/java/edu/muntoclone/MuntoCloneApplication.java
@@ -0,0 +1,13 @@
+package edu.muntoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuntoCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuntoCloneApplication.class, args);
+    }
+
+}
