92f43675778c0ce3590cd135c26c37ccc7669eb7
coldrain-f <intmain__@naver.com>
2022-06-19 22:57:26 +0900

    소셜링 참여 API 기본 로직 구현

--- src/main/java/edu/muntoclone/controller/SocialApiController.java
+++ src/main/java/edu/muntoclone/controller/SocialApiController.java
@@ -6,9 +6,11 @@ import edu.muntoclone.dto.SocialRegisterRequest;
 import edu.muntoclone.entity.Member;
 import edu.muntoclone.entity.Participation;
 import edu.muntoclone.entity.Social;
+import edu.muntoclone.repository.ParticipationRepository;
 import edu.muntoclone.security.PrincipalDetails;
 import edu.muntoclone.service.ParticipationService;
 import edu.muntoclone.service.SocialService;
+import edu.muntoclone.type.RecruitmentType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @RestController
@@ -29,6 +30,7 @@ public class SocialApiController {
 
     private final SocialService socialService;
     private final ParticipationService participationService;
+    private final ParticipationRepository participationRepository;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/socials", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -62,16 +64,19 @@ public class SocialApiController {
 
     @GetMapping("/socials/{id}/members")
     public SocialMembersResponse findAllSocialMembers(@PathVariable Long id) {
+        // TODO: 2022-06-19 서비스 로직으로 분리해야 함.
         final Member owner = socialService.findById(id).getOwner();
-        final List<Participation> participationList = participationService.findAllBySocialId(id);
+        final List<Participation> participationList = participationService.findAllBySocialId(id)
+                .stream().filter(p -> p.getApprovedStatus().equals(1))
+                .collect(toList());
         final List<Member> members = participationList.stream()
                 .map(Participation::getMember)
-                .collect(Collectors.toList());
+                .collect(toList());
 
         final List<SocialMembersResponse.SocialMember> socialMemberResponses =
                 members.stream()
-                .map(SocialMembersResponse.SocialMember::of)
-                .collect(Collectors.toList());
+                        .map(SocialMembersResponse.SocialMember::of)
+                        .collect(toList());
 
         return SocialMembersResponse.builder()
                 .owner(SocialMembersResponse.SocialMember.of(owner))
@@ -81,7 +86,32 @@ public class SocialApiController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/socials/{id}/participation")
-    public void participate(@PathVariable Long id) {
-        // TODO document why this method is empty
+    public void participate(@PathVariable Long id,
+                            @RequestBody String answer,
+                            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        final Social social = socialService.findById(id);
+        final Member participationMember = principalDetails.getMember();
+        int approvedStatus = 0;
+
+        // TODO: 2022-06-19 서비스 로직으로 분리해야 함.
+        final int participationHeadcount = participationService.findAllBySocialId(social.getId())
+                .size();
+        // 소셜링의 참여 인원이 꽉찼으면 거부
+        if (participationHeadcount >= social.getLimitHeadcount()) {
+            // 참여 불가능 -> Exception
+        }
+
+        if (social.getRecruitmentType() == RecruitmentType.EARLY_BIRD) {
+            approvedStatus = 1;
+        }
+
+        final Participation participation = Participation.builder()
+                .member(participationMember)
+                .social(social)
+                .answer(answer)
+                .approvedStatus(approvedStatus)
+                .build();
+
+        participationRepository.save(participation);
     }
 }
--- src/main/java/edu/muntoclone/entity/Participation.java
+++ src/main/java/edu/muntoclone/entity/Participation.java
@@ -1,6 +1,7 @@
 package edu.muntoclone.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -30,4 +31,11 @@ public class Participation {
     @JoinColumn(name = "SOCIAL_ID")
     private Social social;
 
+    @Builder
+    public Participation(String answer, Integer approvedStatus, Member member, Social social) {
+        this.answer = answer;
+        this.approvedStatus = approvedStatus;
+        this.member = member;
+        this.social = social;
+    }
 }
